b35bd69531565525f177a6f57b0b511aef8617d4
Yerin Song <yrsong0915@gmail.com>
2023-11-11 15:29:37 +0900

    [add] 비밀번호 입력 액티비티 생성

---
+++ app/src/main/java/com/ssu/bilda/PwInputActivity.kt
@@ -0,0 +1,11 @@
+package com.ssu.bilda
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PwInputActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pw_input)
+    }
+}
