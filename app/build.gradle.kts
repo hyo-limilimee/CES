5776dfb37e5f5f0cf6eb10e125a0f4e6cc9f56b8
Yerin Song <yrsong0915@gmail.com>
2023-11-14 02:43:14 +0900

    [setting] 리싸이클러뷰 dependency 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,8 @@ android {
 dependencies {
 
     //recyclerview
-    implementation ("androidx.recyclerview:recyclerview:1.3.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.recyclerview:recyclerview-selection:1.1.0")
 
     //material design
     implementation ("com.google.android.material:material:1.1.0")
