3be43941c3341197a5f66b30ddd74923656cd76a
Yerin Song <yrsong0915@gmail.com>
2023-11-13 17:03:39 +0900

    [setting] material design 사용 dependency 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,8 @@ dependencies {
     //recyclerview
     implementation ("androidx.recyclerview:recyclerview:1.3.0")
 
+    //material design
+    implementation ("com.google.android.material:material:1.1.0")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
