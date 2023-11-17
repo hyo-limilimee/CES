9d8f47ae175ee8e7c0aa6b6de9a90bd67df8c8fb
hyo-limilimee <1001lily@naver.com>
2023-11-18 00:39:50 +0900

    [ui] 팀원 평가 페이지 완성

--- app/src/main/java/com/ssu/bilda/TeammateEvalutionFragment.kt
+++ app/src/main/java/com/ssu/bilda/TeammateEvalutionFragment.kt
@@ -1,23 +1,36 @@
-package com.ssu.bilda
-
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-class TeammateEvalutionFragment : Fragment() {
+import android.widget.FrameLayout
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.ssu.bilda.R
+import github.hongbeomi.dividerseekbar.DividerSeekBar
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
-    }
+class TeammateEvalutionFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_teammate_evalution, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_teammate_evalution, container, false)
+
+        // Initialize DividerSeekBar
+        val dividerSeekBar = DividerSeekBar(requireContext()).apply {
+            max = 100
+            // ... (Set other properties)
+        }
+
+        // Set additional properties after initializing the DividerSeekBar
+        dividerSeekBar.setTextLocationMode(DividerSeekBar.TEXT_LOCATION_BOTTOM)
+
+        // Find the FrameLayout container in the layout
+        val frameLayoutContainer: FrameLayout = rootView.findViewById(R.id.fl_seek_bar_container)
+
+        // Add DividerSeekBar to FrameLayout
+        frameLayoutContainer.addView(dividerSeekBar)
+
+        return rootView
     }
 }
