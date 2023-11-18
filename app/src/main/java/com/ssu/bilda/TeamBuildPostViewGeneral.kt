fd51c0c28741d2a6599fc58470744092a39dcab5
hyo-limilimee <1001lily@naver.com>
2023-11-18 20:24:34 +0900

    [ui] 팀 빌딩 글 조회 (팀장 입장) 완성

--- app/src/main/java/com/ssu/bilda/TeamBuildPostViewGeneral.kt
+++ app/src/main/java/com/ssu/bilda/TeamBuildPostViewGeneral.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [TeamBuildRequestJoinFragment.newInstance] factory method to
+ * Use the [TeamBuildPostViewGeneral.newInstance] factory method to
  * create an instance of this fragment.
  */
-class TeamBuildRequestJoinFragment : Fragment() {
+class TeamBuildPostViewGeneral : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class TeamBuildRequestJoinFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_team_build_request_join, container, false)
+        return inflater.inflate(R.layout.fragment_team_build_post_view_general, container, false)
     }
 
     companion object {
@@ -49,7 +49,7 @@ class TeamBuildRequestJoinFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            TeamBuildRequestJoinFragment().apply {
+            TeamBuildPostViewGeneral().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
