2ea94b91abb764067beb72c585fc6ebb1355dd9f
Yerin Song <yrsong0915@gmail.com>
2023-11-18 15:13:15 +0900

    [feat] 약관->이메일입력 액티비티 이동 기능 구현

--- app/src/main/java/com/ssu/bilda/TermsActivity.kt
+++ app/src/main/java/com/ssu/bilda/TermsActivity.kt
@@ -1,11 +1,24 @@
 package com.ssu.bilda
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ssu.bilda.databinding.ActivityTermsBinding
 
 class TermsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTermsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_terms)
+        binding = ActivityTermsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //모두 동의 버튼 클릭
+        binding.btnTermsAgree.setOnClickListener {
+            val intent = Intent(this, EmailInputActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            finish()
+        }
     }
 }
