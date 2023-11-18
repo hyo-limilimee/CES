21c2b8246351c3903ef332cce99c59232f144780
Yerin Song <yrsong0915@gmail.com>
2023-11-18 14:00:06 +0900

    [feat] 스플래시 구현 완료

--- app/src/main/java/com/ssu/bilda/SplashActivity.kt
+++ app/src/main/java/com/ssu/bilda/SplashActivity.kt
@@ -1,11 +1,21 @@
 package com.ssu.bilda
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
+    private val SPLASH_DELAY: Long = 3000 // 3초
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val intent = Intent(this, SignInActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            finish()
+        }, SPLASH_DELAY)
     }
 }
