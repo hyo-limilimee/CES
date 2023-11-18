3f85230ccb3213b03d833828c6556198c1e50ea7
Yerin Song <yrsong0915@gmail.com>
2023-11-18 14:43:17 +0900

    [add] 서비스이용약관, 개인정보처리방침 액티비티 생성

---
+++ app/src/main/java/com/ssu/bilda/TermsOfUseActivity.kt
@@ -0,0 +1,11 @@
+package com.ssu.bilda
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TermsOfUseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_terms_of_use)
+    }
+}
