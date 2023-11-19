f249d82dd9c7d4dd1d56da2caa80189b0e357e54
Yerin Song <yrsong0915@gmail.com>
2023-11-19 14:13:22 +0900

    [feat] 다이얼로그 띄우기 및 로그인 화면으로 전환 기능 추가

--- app/src/main/java/com/ssu/bilda/MyInfoInputActivity.kt
+++ app/src/main/java/com/ssu/bilda/MyInfoInputActivity.kt
@@ -1,49 +1,93 @@
 package com.ssu.bilda
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Spinner
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.ssu.bilda.databinding.ActivityMyInfoInputBinding
 
 class MyInfoInputActivity : AppCompatActivity() {
-    private lateinit var spinnerSignupMajor: Spinner
+    private lateinit var binding: ActivityMyInfoInputBinding
     private var isFirstSelection = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_info_input)
-
-        spinnerSignupMajor = findViewById(R.id.spinner_signup_major)
+        binding = ActivityMyInfoInputBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val majors = resources.getStringArray(R.array.major_array)
-
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, majors)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinnerSignupMajor.adapter = adapter
+        binding.spinnerSignupMajor.setSelection(adapter.getPosition("컴퓨터학부"))
 
-        spinnerSignupMajor.adapter = adapter
+        binding.spinnerSignupMajor.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    if (!isFirstSelection) {
+                        val selectedItem = parent?.getItemAtPosition(position).toString()
+                        Toast.makeText(
+                            applicationContext,
+                            "선택한 학과 : $selectedItem",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        isFirstSelection = false
+                    }
+                }
 
-        // 기본값 = 컴퓨터학부
-        spinnerSignupMajor.setSelection(adapter.getPosition("컴퓨터학부"))
-
-        // spinner 관련
-        spinnerSignupMajor.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                if (!isFirstSelection) {
-                    val selectedItem = parent?.getItemAtPosition(position).toString()
-                    Toast.makeText(applicationContext, "선택한 학과: $selectedItem", Toast.LENGTH_SHORT).show()
-                } else {
-                    isFirstSelection = false
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    if (!isFirstSelection) {
+                        Toast.makeText(applicationContext, "학과를 선택해주세요.", Toast.LENGTH_SHORT).show()
+                    }
                 }
             }
-            // spinner 아무것도 선택 안된 경우
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                if (!isFirstSelection) {
-                    Toast.makeText(applicationContext, "학과를 선택해주세요.", Toast.LENGTH_SHORT).show()
-                }
+
+        binding.btnSignupEnd.setOnClickListener {
+            val name = binding.etSignupName.text.toString()
+            val studentId = binding.etSignupStudentid.text.toString()
+            val nickname = binding.etSignupNickname.text.toString()
+
+            if (name.isNotEmpty() && studentId.isNotEmpty() && nickname.isNotEmpty()) {
+                showConfirmDialog()
+            } else {
+                Toast.makeText(this, "모든 정보를 입력해주세요", Toast.LENGTH_SHORT).show()
             }
         }
     }
+
+    // 학번, 이름 확인 관련 다이얼로그
+    private fun showConfirmDialog() {
+        val alertDialog: AlertDialog = AlertDialog.Builder(this@MyInfoInputActivity)
+            .setIcon(R.drawable.ic_notice)
+            .setMessage("입력한 이름과 학번은 추후 수정이 불가합니다.\n 회원가입 완료로 넘어 가시겠어요?")
+            .setPositiveButton("네") { dialog, _ ->
+                dialog.dismiss()
+                // SignInActivity로 이동
+                val intent = Intent(this@MyInfoInputActivity, SignInActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(intent)
+
+                finish()
+            }
+            .setNegativeButton("아니오") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .create()
+
+        alertDialog.setTitle("확인 필요")
+        alertDialog.setCancelable(false)
+        alertDialog.show()
+    }
 }
+
+
