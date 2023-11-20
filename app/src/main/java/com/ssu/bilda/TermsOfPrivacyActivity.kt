0e9fc32ab803748b7a78d9801c7b5f96563322b7
Yerin Song <yrsong0915@gmail.com>
2023-11-21 00:48:50 +0900

    [feat] 개인정보처리방침 -> 약관 페이지 돌아가기 기능 구현

--- app/src/main/java/com/ssu/bilda/TermsOfPrivacyActivity.kt
+++ app/src/main/java/com/ssu/bilda/TermsOfPrivacyActivity.kt
@@ -2,10 +2,24 @@ package com.ssu.bilda
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ssu.bilda.databinding.ActivityMyInfoInputBinding
+import com.ssu.bilda.databinding.ActivityTermsBinding
+import com.ssu.bilda.databinding.ActivityTermsOfPrivacyBinding
 
 class TermsOfPrivacyActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTermsOfPrivacyBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_terms_of_privacy)
+        binding = ActivityTermsOfPrivacyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 버튼 클릭으로 되돌아가기
+        binding.btnPrivacyTermsBack.setOnClickListener {
+            finish()
+        }
+        // 버튼이 담긴 영역 클릭으로 되돌아가기
+        binding.flPrivacyTermsBack.setOnClickListener {
+            finish()
+        }
     }
 }
