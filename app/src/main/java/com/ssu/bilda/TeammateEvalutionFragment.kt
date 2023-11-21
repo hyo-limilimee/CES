bb7338e3c23b6dc30d0dda90e1c93aec1825ee9f
hyo-limilimee <1001lily@naver.com>
2023-11-21 23:26:09 +0900

    [feat] 다시 팀플할 지 여부 물어보는 다이얼로그 띄우기 완성

--- app/src/main/java/com/ssu/bilda/TeammateEvalutionFragment.kt
+++ app/src/main/java/com/ssu/bilda/TeammateEvalutionFragment.kt
@@ -1,3 +1,4 @@
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -31,6 +32,38 @@ class TeammateEvalutionFragment : Fragment() {
         // Add DividerSeekBar to FrameLayout
 //        frameLayoutContainer.addView(dividerSeekBar)
 
+        // "저장" 버튼을 위한 FrameLayout 찾기
+        val saveButton: FrameLayout = rootView.findViewById(R.id.fl_blue_save_btn)
+
+        // "저장" 버튼에 클릭 리스너 설정
+        saveButton.setOnClickListener {
+            // "저장" 버튼을 클릭했을 때 대화 상자 표시
+            showSaveDialog()
+        }
+
         return rootView
     }
+
+    private fun showSaveDialog() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("저장") // 대화 상자의 제목 설정
+        builder.setMessage("이 팀원과 다시 한 번 팀플 하시겠습니까?") // 메시지 설정
+
+        // 대화 상자에 버튼 추가
+        builder.setPositiveButton("네") { dialog, which ->
+            // "저장" 버튼 클릭 처리 (저장 동작)
+            // 여기에 저장 로직을 추가할 수 있습니다.
+            dialog.dismiss() // 대화 상자 닫기
+        }
+
+        builder.setNegativeButton("아니오") { dialog, which ->
+            // "취소" 버튼 클릭 처리 (취소 동작)
+            dialog.dismiss() // 대화 상자 닫기
+        }
+
+        // 대화 상자 생성 및 표시
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
 }
