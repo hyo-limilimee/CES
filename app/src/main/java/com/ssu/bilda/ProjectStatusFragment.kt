7920d1260cb0620f4a5045dea7cf6979a852a9c9
hyo-limilimee <1001lily@naver.com>
2023-11-21 20:35:45 +0900

    [feat] 평가하기 과목 리스트에서 팀원 이름 리스트로 전환 연결

--- app/src/main/java/com/ssu/bilda/ProjectStatusFragment.kt
+++ app/src/main/java/com/ssu/bilda/ProjectStatusFragment.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.ssu.bilda.R
+import com.ssu.bilda.SubjectStatusFragment
 
 class ProjectStatusFragment : Fragment() {
 
@@ -13,20 +14,31 @@ class ProjectStatusFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // 프래그먼트의 레이아웃을 인플레이트합니다.
+        // fragment layout inflate
         val view = inflater.inflate(R.layout.fragment_project_status, container, false)
 
-        // RecyclerView에 들어갈 샘플 데이터
+        // recyclerview 더미 데이터
         val subjectList = listOf("과목 1", "과목 2", "과목 3")
 
-        // 어댑터를 생성하고 설정합니다.
+        // adapter 생성, 설정
         val adapter = SubjectAdapter(requireContext(), subjectList)
 
-        // RecyclerView를 찾아 초기화하고 어댑터를 설정합니다.
+        // RecyclerView를 초기화, adapter 설정
         val rvSubjectList: RecyclerView = view.findViewById(R.id.rv_subject_list)
         rvSubjectList.layoutManager = LinearLayoutManager(requireContext())
         rvSubjectList.adapter = adapter
 
+        // SubjectAdapter의 클릭 리스너 설정
+        adapter.setOnItemClickListener { view ->
+            // 아이템이 클릭되었을 때 수행할 동작 구현
+            replaceFragment(SubjectStatusFragment())
+        }
         return view
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.fl_content, fragment)
+            .commit()
     }
 }
--- app/src/main/java/com/ssu/bilda/SubjectAdapter.kt
+++ app/src/main/java/com/ssu/bilda/SubjectAdapter.kt
@@ -2,33 +2,54 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
+import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.ssu.bilda.R
+import com.ssu.bilda.SubjectStatusFragment
 
 class SubjectAdapter(private val context: Context, private val subjectList: List<String>) :
+// RecyclerView.Adapter 클래스 상속 받음 & subjetViewHolder -> 제네릭
     RecyclerView.Adapter<SubjectAdapter.SubjectViewHolder>() {
 
+    private var onItemClick: ((View) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (View) -> Unit) {
+        onItemClick = listener
+    }
+
+    // 새로운 아이템 뷰 위한 뷰홀더 객체 생성, 초기화
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SubjectViewHolder {
         val view =
             LayoutInflater.from(context).inflate(R.layout.item_rcv_evalution_subject, parent, false)
+        // 만들어진 뷰 객체 -> SubjectViewHolder의 인스턴스로 감싸서 반환
         return SubjectViewHolder(view)
     }
 
+    // 특정 위치의 데이터를 뷰 홀더에 바인딩
     override fun onBindViewHolder(holder: SubjectViewHolder, position: Int) {
         val subject = subjectList[position]
         holder.bind(subject)
     }
 
+    // 데이터의 총 아이템 수 반환
     override fun getItemCount(): Int {
         return subjectList.size
     }
 
+    //아이템의 각 뷰 홀더, RecyclerView.ViewHolder 클래스 상속 받음
     inner class SubjectViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val textView: TextView = itemView.findViewById(R.id.tv_evaluation_subject)
+        private val rightArrowLayout: FrameLayout =
+            itemView.findViewById(R.id.fl_ic_gray_right_arrow)
 
         fun bind(subject: String) {
             textView.text = subject
+
+            rightArrowLayout.setOnClickListener {
+                onItemClick?.invoke(rightArrowLayout)
+            }
         }
     }
 }
