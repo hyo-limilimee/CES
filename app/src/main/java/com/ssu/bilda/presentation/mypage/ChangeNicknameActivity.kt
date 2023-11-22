e3a8935db320bee509a982828f103ff56005117a
hyo-limilimee <1001lily@naver.com>
2023-11-23 02:21:53 +0900

    [feat] 마이페이지 뒤로 가기 버튼 활성화

--- app/src/main/java/com/ssu/bilda/presentation/mypage/ChangeNicknameActivity.kt
+++ app/src/main/java/com/ssu/bilda/presentation/mypage/ChangeNicknameActivity.kt
@@ -2,11 +2,19 @@ package com.ssu.bilda.presentation.mypage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.ssu.bilda.R
 
 class ChangeNicknameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_change_nickname)
+
+        val llArrowAndChangePw: View = findViewById(R.id.ll_ic_arrow_and_change_nickname)
+        // ll_ic_arrow_and_change_nickname을 클릭할 때의 이벤트 리스너 설정
+        llArrowAndChangePw.setOnClickListener {
+            // MyInfoFragment로 돌아가기
+            onBackPressed()
+        }
     }
 }
