44a1adde204479491313f02d3029d3cae0b8a781
bqqmf <39626552+bqqmf@users.noreply.github.com>
2023-11-24 02:24:42 +0900

    Update Product.java

--- WebTicket/src/dto/Product.java
+++ WebTicket/src/dto/Product.java
@@ -2,108 +2,90 @@ package dto;
 
 import java.io.Serializable;
 
-public class Product implements Serializable {
+public class Movie implements Serializable {
 
-	private static final long serialVersionUID = -4274700572038677000L;
+private static final long serialVersionUID = -4274700572038677000L;
 
-	private String productId;
-	private String pname;
-	private Integer unitPrice;
-	private String description;
-	private String manufacturer;
-	private String category;
-	private long unitsInStock;
-	private String condition;
-	private String filename;
-	private int quantity;
-
-	public Product() {
+	private String movieId; 	// 영화 id
+	private String title;  		// 영화 제목
+	private int price;		// 영화 가격
+	private String description;	// 영화 설명
+	private String manufacturer;	// 제작사
+	private String genre;		// 장르 
+	private int remainingSeats;	// 남은 좌석 수 
+	private int totalSeats;		// 총 좌석 수
+	
+	public Movie() {
 		super();
 	}
-
-	public Product(String productId, String pname, Integer unitPrice) {
-		this.productId = productId;
-		this.pname = pname;
-		this.unitPrice = unitPrice;
+	
+	public Movie(String movieId, String title, int price) {
+		this.movieId = movieId;
+		this.title = title;
+		this.price = price;
 	}
-
-	public String getProductId() {
-		return productId;
+	
+	public String getMovieId() {
+		return movieId;
+	    }
+		
+	public void setMovieId(String movieId) {
+	this.movieId = movieId;
 	}
-
-	public String getPname() {
-		return pname;
+	
+	public String getTitle() {
+	return title;
 	}
-
-	public void setPname(String pname) {
-		this.pname = pname;
+	
+	public void setTitle(String title) {
+	this.title = title;
 	}
-
-	public void setProductId(String productId) {
-		this.productId = productId;
+	
+	public int getPrice() {
+	return price;
 	}
-
-	public Integer getUnitPrice() {
-		return unitPrice;
+	
+	public void setPrice(int price) {
+	this.price = price;
 	}
-
-	public void setUnitPrice(Integer unitPrice) {
-		this.unitPrice = unitPrice;
-	}
-
+	
 	public String getDescription() {
-		return description;
+	return description;
 	}
-
+	
 	public void setDescription(String description) {
-		this.description = description;
+	this.description = description;
 	}
-
+	
 	public String getManufacturer() {
-		return manufacturer;
+	return manufacturer;
 	}
-
+	
 	public void setManufacturer(String manufacturer) {
-		this.manufacturer = manufacturer;
-	}
-
-	public String getCategory() {
-		return category;
-	}
-
-	public void setCategory(String category) {
-		this.category = category;
-	}
-
-	public long getUnitsInStock() {
-		return unitsInStock;
-	}
-
-	public void setUnitsInStock(long unitsInStock) {
-		this.unitsInStock = unitsInStock;
-	}
-
-	public String getCondition() {
-		return condition;
-	}
-
-	public void setCondition(String condition) {
-		this.condition = condition;
+	this.manufacturer = manufacturer;
 	}
 	
-	public String getFilename() {
-		return filename;
-	}
-
-	public void setFilename(String filename) {
-		this.filename = filename;
+	public String getGenre() {
+	return genre;
 	}
 	
-	public int getQuantity() {
-		return quantity;
+	public void setGenre(String genre) {
+	this.genre = genre;
 	}
-
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
+	
+	public int getRemainingSeats() {
+	return remainingSeats;
+	}
+	
+	public void setRemainingSeats(int remainingSeats) {
+	this.remainingSeats = remainingSeats;
+	}
+	
+	public int getTotalSeats() {
+	return totalSeats;
+	}
+	
+	public void setTotalSeats(int totalSeats) {
+	this.totalSeats = totalSeats;
 	}
 }
