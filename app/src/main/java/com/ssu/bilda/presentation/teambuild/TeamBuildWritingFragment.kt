cf77664204fe473714283cc7542b6fc209817662
hyo-limilimee <1001lily@naver.com>
2023-11-25 17:34:59 +0900

    [feat] 임시버튼 세팅 - 평가하기 페이지 좌상단

--- app/src/main/java/com/ssu/bilda/presentation/teambuild/TeamBuildWritingFragment.kt
+++ app/src/main/java/com/ssu/bilda/presentation/teambuild/TeamBuildWritingFragment.kt
@@ -29,7 +29,7 @@ class TeamBuildWritingFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_team_build_writing, container, false)
     }
 
