6ccc9dc6f923c697ae7807bf52f87d6e92741c30
hyo-limilimee <1001lily@naver.com>
2023-11-25 18:15:29 +0900

    [feat] 작성 완료 버튼 다이얼로그 추가

--- app/src/main/java/com/ssu/bilda/presentation/teambuild/TeamBuildWritingFragment.kt
+++ app/src/main/java/com/ssu/bilda/presentation/teambuild/TeamBuildWritingFragment.kt
@@ -1,5 +1,6 @@
 package com.ssu.bilda.presentation.teambuild
 
+import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -27,6 +28,14 @@ class TeamBuildWritingFragment : Fragment() {
             showDatePickerDialog(tvSelectedDate)
         }
 
+        val doneBtn: FrameLayout = view.findViewById(R.id.fl_ic_teambuild_writing_done_btn)
+
+        // "작성완료" 버튼에 클릭 리스너 설정
+        doneBtn.setOnClickListener {
+
+            showDoneDialog()
+        }
+
         return view
     }
 
@@ -49,5 +58,27 @@ class TeamBuildWritingFragment : Fragment() {
         datePickerDialog.datePicker.maxDate = System.currentTimeMillis()
 
         datePickerDialog.show()
+    }
+
+    private fun showDoneDialog() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("") // 대화 상자의 제목 설정
+        builder.setMessage("한 번 작성한 글은 수정이 불가능합니다. 작성 완료하시겠습니까?") // 메시지 설정
+
+        // 대화 상자에 버튼 추가
+        builder.setPositiveButton("네") { dialog, which ->
+            // "저장" 버튼 클릭 처리 (저장 동작)
+            // 여기에 저장 로직을 추가할 수 있습니다.
+            dialog.dismiss() // 대화 상자 닫기
+        }
+
+        builder.setNegativeButton("아니오") { dialog, which ->
+            // "취소" 버튼 클릭 처리 (취소 동작)
+            dialog.dismiss() // 대화 상자 닫기
+        }
+
+        // 대화 상자 생성 및 표시
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
     }
 }
