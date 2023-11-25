5f1b6ff7ec29e679e22abc70fc01aaba72638088
hyo-limilimee <1001lily@naver.com>
2023-11-25 18:21:37 +0900

    [feat] 모집할 인원 수 선택 스피너 구현

--- app/src/main/java/com/ssu/bilda/presentation/teambuild/TeamBuildWritingFragment.kt
+++ app/src/main/java/com/ssu/bilda/presentation/teambuild/TeamBuildWritingFragment.kt
@@ -6,7 +6,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.FrameLayout
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.ssu.bilda.R
@@ -35,6 +37,17 @@ class TeamBuildWritingFragment : Fragment() {
 
             showDoneDialog()
         }
+
+        // spinner 데이터
+        val data = listOf("2명","3명","4명","5명","6명")
+
+        // Spinner initialization
+        val spinner: Spinner = view.findViewById(R.id.spinner_profile_select_member_num)
+
+        // Adapter creation and data setting
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, data)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
 
         return view
     }
