52fd1211dba250d163249ec489f9c6b4e48d8f28
hyo-limilimee <1001lily@naver.com>
2023-11-28 22:27:28 +0900

    [feat] mp 차트 기본 구현 완료

--- app/src/main/java/com/ssu/bilda/presentation/mypage/ProfileFragment.kt
+++ app/src/main/java/com/ssu/bilda/presentation/mypage/ProfileFragment.kt
@@ -1,28 +1,58 @@
 package com.ssu.bilda.presentation.mypage
 
+import android.graphics.Color
 import android.os.Bundle
-import android.text.TextUtils.replace
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import com.github.mikephil.charting.charts.RadarChart
+import com.github.mikephil.charting.data.RadarData
+import com.github.mikephil.charting.data.RadarDataSet
+import com.github.mikephil.charting.data.RadarEntry
+import com.github.mikephil.charting.utils.ColorTemplate
 import com.ssu.bilda.R
-import com.ssu.bilda.databinding.FragmentProfileBinding
-import com.ssu.bilda.presentation.BnvActivity
-import com.ssu.bilda.presentation.evaluate.ProjectStatusFragment
-import com.ssu.bilda.presentation.evaluate.SubjectStatusFragment
-import com.ssu.bilda.presentation.home.HomeFragment
 
 class ProfileFragment : Fragment() {
+
+    lateinit var radarChart: RadarChart
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
 
+        // mp 차트
+        radarChart = view.findViewById(R.id.fl_mp_chart_my_score)
+
+        val list:ArrayList<RadarEntry> = ArrayList()
+
+        list.add(RadarEntry(100f))
+        list.add(RadarEntry(101f))
+        list.add(RadarEntry(102f))
+        list.add(RadarEntry(103f))
+        list.add(RadarEntry(104f))
+        
+        val radarDataSet = RadarDataSet(list, "List")
+
+        radarDataSet.setColors(ColorTemplate.MATERIAL_COLORS,255)
+
+        radarDataSet.lineWidth=2f
+
+        radarDataSet.valueTextSize=14f
+
+        radarDataSet.valueTextColor = Color.RED
+
+        val radarData = RadarData()
+
+        radarData.addDataSet(radarDataSet)
+
+        radarChart.description.text = "Radar Chart"
+        
+        radarChart.animateY(2000)
+
+        //  설정 버튼에 대한 화면 전환 처리
         val settingButton: FrameLayout = view.findViewById(R.id.fl_ic_setting_btn)
 
         settingButton.setOnClickListener { view ->
@@ -38,32 +68,3 @@ class ProfileFragment : Fragment() {
             .commit()
     }
 }
-
-//    private fun dataValue(): ArrayList<RadarEntry> {
-//        val dataVals = ArrayList<RadarEntry>()
-//        dataVals.add(RadarEntry(bigMartList.size.toFloat()))
-//        dataVals.add(RadarEntry(gs24List.size.toFloat()))
-//        dataVals.add(RadarEntry(schoolList.size.toFloat()))
-//        dataVals.add(RadarEntry(academyList.size.toFloat()))
-//        dataVals.add(RadarEntry(subwayList.size.toFloat()))
-//        dataVals.add(RadarEntry(bankList.size.toFloat()))
-//        dataVals.add(RadarEntry(hospitalList.size.toFloat()))
-//        dataVals.add(RadarEntry(pharmacyList.size.toFloat()))
-//        dataVals.add(RadarEntry(cafeList.size.toFloat()))
-//        return dataVals
-//    }
-//
-//    private fun makeChart() {
-//        val dataSet = RadarDataSet(dataValue(), "DATA")
-//        dataSet.color = Color.BLUE
-//
-//        val data = RadarData()
-//        data.addDataSet(dataSet)
-//        val labels = arrayOf("대형마트", "편의점", "학교", "학원", "지하철", "은행", "병원", "약국", "카페")
-//        val xAxis: XAxis = radarChart.xAxis
-//        xAxis.valueFormatter = IndexAxisValueFormatter(labels)
-//        radarChart.data = data
-
-
-//    }
-
