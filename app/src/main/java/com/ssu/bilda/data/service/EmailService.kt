61ebf3e776c7a3f3f954b23590ab53842ae26c25
Yerin Song <yrsong0915@gmail.com>
2023-11-29 13:12:28 +0900

    [add] 닉네임변경req 파일생성

--- app/src/main/java/com/ssu/bilda/data/service/EmailService.kt
+++ app/src/main/java/com/ssu/bilda/data/service/EmailService.kt
@@ -1,6 +1,9 @@
 package com.ssu.bilda.data.service
 
+import com.ssu.bilda.data.remote.request.VerifyEmailRequest
+import com.ssu.bilda.data.remote.response.BaseResponse
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -9,13 +12,13 @@ interface EmailService {
     // 이메일 전송
     @GET("/api/v1/email/verify")
     fun sendEmail(
-        @Query("email") email: String,
-    ): Call<String>
+        @Body request: VerifyEmailRequest
+    ): Call<BaseResponse<Void>>
 
     // 이메일 인증
     @GET("/api/v1/email/verify/code")
     fun certifyEmail(
         @Query("email") email: String,
         @Query("code") code: String,
-    ): Call<String>
+    ): Call<BaseResponse<Void>>
 }
