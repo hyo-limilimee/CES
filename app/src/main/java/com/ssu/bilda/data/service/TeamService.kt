6f7d849d0afe71bf91442d8950f64e4a3f31f547
Yerin Song <yrsong0915@gmail.com>
2023-11-29 13:18:49 +0900

    [refactor] team서비스 수정

---
+++ app/src/main/java/com/ssu/bilda/data/service/TeamService.kt
@@ -0,0 +1,76 @@
+package com.ssu.bilda.data.service
+
+import com.ssu.bilda.data.common.PendingUser
+import com.ssu.bilda.data.remote.request.CreateTeamRequest
+import com.ssu.bilda.data.remote.request.VerifyEmailRequest
+import com.ssu.bilda.data.remote.response.BaseResponse
+import com.ssu.bilda.data.remote.response.TeamResponse
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface TeamService {
+
+    @GET("/api/v1/email/verify")
+    fun sendEmail(
+        @Body request: VerifyEmailRequest
+    ): Call<BaseResponse<Void>>
+
+
+    // 팀의 정보 가져오기
+    @GET("/api/v1/teams/{teamId}")
+    fun getTeamInfo(@Path("teamId") teamId: Long): Call<BaseResponse<TeamResponse>>
+
+    // 팀 조인 요청 확인하기
+    @GET("/api/v1/teams/{teamId}/recruit")
+    fun getPendingUsers(@Path("teamId") teamId: Long): Call<BaseResponse<List<PendingUser>>>
+
+    //  User가 속해있는 팀들의 정보를 가져오기
+    @GET(" /api/v1/teams/user/{userId}")
+    fun getUserTeams(@Path("userId") userId: Long): Call<BaseResponse<List<TeamResponse>>>
+
+    //  과목에 해당하는 팀들의 정보 가져오기
+    @GET(" /api/v1/teams/subject/{subjectId}")
+    fun getTeamsBySubject(@Path("subjectId") subjectId: Long): Call<BaseResponse<List<TeamResponse>>>
+
+
+    // 팀 조인 요청 거절하기
+    @POST("/api/v1/teams/{teamId}/reject/{pendingUserId}")
+    fun rejectJoinRequest(
+        @Path("teamId") teamId: Long,
+        @Path("pendingUserId") pendingUserId: Long
+    ): Call<BaseResponse<Void>>
+
+
+    // 팀에 조인 요청하기
+    @POST("/api/v1/teams/{teamId}/join/{userId}")
+    fun requestToJoinTeam(
+        @Path("teamId") teamId: Long,
+        @Path("userId") userId: Long
+    ): Call<BaseResponse<Void>>
+
+
+    //  팀 조인 요청 수락하기
+    @POST("/api/v1/teams/{teamId}/approve/{pendingUserId}")
+    fun approveJoinRequest(
+        @Path("teamId") teamId: Long,
+        @Path("pendingUserId") pendingUserId: Long
+    ): Call<BaseResponse<Void>>
+
+
+    //   팀 생성하기
+    @POST("/api/v1/teams/create/{leaderId}")
+    fun createTeam(
+        @Path("leaderId") leaderId: Long,
+        @Body request: CreateTeamRequest
+    ): Call<BaseResponse<Void>>
+
+    //  팀플 종료하기
+    @POST("/api/v1/teams/complete/{teamId}")
+    fun completeTeam(
+        @Path("teamId") teamId: Long
+    ): Call<BaseResponse<Void>>
+
+}
