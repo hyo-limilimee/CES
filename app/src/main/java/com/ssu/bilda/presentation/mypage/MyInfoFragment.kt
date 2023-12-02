1ef5c79b8f7ce5f3af17efeb429cf2cae1bfbbb7
hyo-limilimee <1001lily@naver.com>
2023-12-02 17:17:48 +0900

    [feat] 내 정보 사용자 이메일, 비밀번호 불러오기  구현

--- app/src/main/java/com/ssu/bilda/presentation/mypage/MyInfoFragment.kt
+++ app/src/main/java/com/ssu/bilda/presentation/mypage/MyInfoFragment.kt
@@ -11,8 +11,14 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.ssu.bilda.R
 import com.ssu.bilda.data.remote.App
+import com.ssu.bilda.data.remote.RetrofitImpl
 import com.ssu.bilda.data.remote.UserSharedPreferences
+import com.ssu.bilda.data.remote.response.BaseResponse
+import com.ssu.bilda.data.service.UserService
 import com.ssu.bilda.presentation.sign.SignInActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MyInfoFragment : Fragment() {
     override fun onCreateView(
@@ -35,30 +41,30 @@ class MyInfoFragment : Fragment() {
             // 꼭 commit을 해줘야 바뀐다.
             transaction.commit()
         }
-
-        // 사용자 이름 불러오기
-        val tvUserName: TextView = rootView.findViewById(R.id.tv_my_info_name_input)
-        val userName = UserSharedPreferences.getUserName(requireContext())
-        // 사용자 이름이 있다면 TextView에 설정
-        if (userName.isNotEmpty()) {
-            tvUserName.text = userName
-        }
-
-        // 사용자 학번 불러오기
-        val tvUserSId: TextView = rootView.findViewById(R.id.tv_my_info_student_id_input)
-        val userSId = UserSharedPreferences.getUserStId(requireContext())
-
-        if (userName.isNotEmpty()) {
-            tvUserSId.text = userSId
-        }
-
-        // 사용자 닉네임 불러오기
-        val tvUserNickname: TextView = rootView.findViewById(R.id.tv_my_info_nick_name_input)
-        val userNickname = UserSharedPreferences.getUserNickname(requireContext())
-
-        if (userName.isNotEmpty()) {
-            tvUserNickname.text = userNickname
-        }
+//
+//        // 사용자 이름 불러오기
+//        val tvUserName: TextView = rootView.findViewById(R.id.tv_my_info_name_input)
+//        val userName = UserSharedPreferences.getUserName(requireContext())
+//        // 사용자 이름이 있다면 TextView에 설정
+//        if (userName.isNotEmpty()) {
+//            tvUserName.text = userName
+//        }
+//
+//        // 사용자 학번 불러오기
+//        val tvUserSId: TextView = rootView.findViewById(R.id.tv_my_info_student_id_input)
+//        val userSId = UserSharedPreferences.getUserStId(requireContext())
+//
+//        if (userName.isNotEmpty()) {
+//            tvUserSId.text = userSId
+//        }
+//
+//        // 사용자 닉네임 불러오기
+//        val tvUserNickname: TextView = rootView.findViewById(R.id.tv_my_info_nick_name_input)
+//        val userNickname = UserSharedPreferences.getUserNickname(requireContext())
+//
+//        if (userName.isNotEmpty()) {
+//            tvUserNickname.text = userNickname
+//        }
 
         // 사용자 이메일 불러오기
         val tvUserEmail: TextView = rootView.findViewById(R.id.tv_my_info_email_input)
@@ -152,11 +158,10 @@ class MyInfoFragment : Fragment() {
         builder.setTitle("") // 대화 상자의 제목 설정
         builder.setMessage("정말 탈퇴 하시겠습니까?") // 메시지 설정
 
-        // 대화 상자에 버튼 추가
         builder.setPositiveButton("네") { dialog, which ->
-            // "저장" 버튼 클릭 처리 (저장 동작)
-            // 여기에 저장 로직을 추가할 수 있습니다.
-            dialog.dismiss() // 대화 상자 닫기
+            // "네" 버튼을 클릭했을 때 withdraw 메서드 호출
+            withdraw()
+            dialog.dismiss() // 다이얼로그 닫기
         }
 
         builder.setNegativeButton("아니오") { dialog, which ->
@@ -169,5 +174,29 @@ class MyInfoFragment : Fragment() {
         dialog.show()
     }
 
-
+    private fun withdraw() {
+        // 서버에 회원 탈퇴 요청을 보내는 Retrofit 인스턴스 생성
+//        val retrofit = RetrofitImpl.authenticatedRetrofit
+//        val service = retrofit.create(UserService::class.java)
+//
+//        // 실제 서버 통신을 위한 API 호출
+//        val call = service.withdraw()
+//
+//        call.enqueue(object : Callback<BaseResponse<Any>> {
+//            override fun onResponse(call: Call<BaseResponse<Any>>, response: Response<BaseResponse<Any>>) {
+//                if (response.isSuccessful) {
+//                    // 로그인 화면으로 리다이렉트
+//                    startActivity(Intent(context, SignInActivity::class.java))
+//                } else {
+//                    // 서버로부터 실패한 응답을 받았을 때에 대한 처리를 여기에 추가할 수 있습니다.
+//                    // 예를 들어, 실패한 이유를 사용자에게 알리는 등의 작업을 수행합니다.
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<BaseResponse<Any>>, t: Throwable) {
+//                // 네트워크 오류 등으로 통신 실패 시에 대한 처리를 여기에 추가할 수 있습니다.
+//                // 예를 들어, 실패한 이유를 사용자에게 알리는 등의 작업을 수행합니다.
+//            }
+//        })
+    }
 }
