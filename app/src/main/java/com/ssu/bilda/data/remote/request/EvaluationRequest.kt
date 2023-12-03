d318becce314aff1b267cf6a41062c434cb98e3d
hyo-limilimee <1001lily@naver.com>
2023-12-03 15:06:27 +0900

    [feat] 평가 결과 전송 구현

--- app/src/main/java/com/ssu/bilda/data/remote/request/EvaluationRequest.kt
+++ app/src/main/java/com/ssu/bilda/data/remote/request/EvaluationRequest.kt
@@ -3,8 +3,7 @@ package com.ssu.bilda.data.remote.request
 import Scores
 
 data class EvaluationRequest(
-    val evaluatedUserId: Long,
-    val teamId: Long,
+    val evaluatedUserId: Int,
+    val teamId: Int,
     val scores: Scores
 )
-
--- app/src/main/java/com/ssu/bilda/data/service/EvaluationService.kt
+++ app/src/main/java/com/ssu/bilda/data/service/EvaluationService.kt
@@ -1,9 +1,8 @@
 package com.ssu.bilda.data.service
 
-import com.ssu.bilda.data.common.SubjectWithTeamStatus
+import com.ssu.bilda.data.common.User
 import com.ssu.bilda.data.remote.request.EvaluationRequest
 import com.ssu.bilda.data.remote.response.BaseResponse
-import com.ssu.bilda.data.remote.response.SubjectResponse
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.Body
@@ -11,13 +10,12 @@ import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface EvaluationService {
-    //유저가 속해있는 과목 정보 가져오기
-    @GET("/api/v1/subject")
-    fun getUserSubjects(): Response<SubjectResponse>
 
+//    // 유저가 수강하는 과목 불러오기
+//    @GET("/api/v1/subject")
+//    suspend fun getUserSubjects(): Response<SubjectResponse>
 
-    // 평가 점수 돌려주기
-    @POST("your_endpoint_path")
-    fun sendEvaluationRequest(@Body request: EvaluationRequest): Call<EvaluationRequest>
-
+    // 평가 요청 보내기
+    @POST("/api/v1/evaluation") // replace with your actual endpoint path
+    fun sendEvaluationRequest(@Body request: EvaluationRequest): Call<BaseResponse<User>>
 }
