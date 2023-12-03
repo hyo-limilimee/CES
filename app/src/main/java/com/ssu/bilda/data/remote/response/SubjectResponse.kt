d385f4e5fe2efb8c9ac01038af0cd4e8a5389b6a
hyo-limilimee <1001lily@naver.com>
2023-12-03 16:16:08 +0900

    [feat] 인터페이스 데이터 클래스 생성

---
+++ app/src/main/java/com/ssu/bilda/data/remote/response/SubjectResponse.kt
@@ -0,0 +1,10 @@
+package com.ssu.bilda.data.remote.response
+
+import com.ssu.bilda.data.common.SubjectWithTeamStatus
+
+data class SubjectResponse(
+    val success: Boolean,
+    val code: Int,
+    val message: String,
+    val result: List<SubjectWithTeamStatus>
+)
