d194daec983a13bcd2504066389f225163ec4f69
hyo-limilimee <1001lily@naver.com>
2023-12-03 22:05:02 +0900

    [fix] 팀원 평가 넘어가기 성공

--- app/src/main/java/com/ssu/bilda/presentation/evaluate/SubjectStatusFragment.kt
+++ app/src/main/java/com/ssu/bilda/presentation/evaluate/SubjectStatusFragment.kt
@@ -37,35 +37,24 @@ class SubjectStatusFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // fragment layout inflate
         val view = inflater.inflate(R.layout.fragment_subject_status, container, false)
         val title = arguments?.getString("title", "") ?: ""
 
-        // 과목 이름 세팅
         val tvSubjectName: TextView = view.findViewById(R.id.tv_subject_name)
         tvSubjectName.text = title
 
-        // 사용자 이름 세팅
         val tvEvaluationName: TextView = view.findViewById(R.id.tv_evaluation_name)
         val userName = UserSharedPreferences.getUserName(requireContext())
         tvEvaluationName.text = userName
 
-        //리싸이클러뷰 세팅
         val recyclerView: RecyclerView = view.findViewById(R.id.rv_teammate_name_list)
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         teammateNameAdapter = TeammateNameAdapter(requireContext(), emptyList())
         recyclerView.adapter = teammateNameAdapter
 
-        // Fetch teammate names directly without ViewModel
         fetchTeammateNames()
 
         return view
-    }
-
-    private fun replaceFragment(fragment: Fragment) {
-        requireActivity().supportFragmentManager.beginTransaction()
-            .replace(R.id.fl_content, fragment)
-            .commit()
     }
 
     private fun fetchTeammateNames() {
@@ -73,26 +62,27 @@ class SubjectStatusFragment : Fragment() {
             try {
                 val response = RetrofitImpl.authenticatedRetrofit
                     .create(EvaluationService::class.java)
-                    .getUserTeamsWithMembers()
+                    .getUserTeams()
+
+                Log.d("SubjectStatusFragment", "Raw Response: ${response.raw()}")
 
                 if (response.isSuccessful) {
-                    val teamsWithMembers = response.body()
+                    val teamsWithMembers = response.body()?.result
                     teamsWithMembers?.let {
                         val teamMembers = it.flatMap(EvaluationTeam::members)
                         teammateNameAdapter.updateData(teamMembers.map { member -> member.name })
 
-                        // Log the response body
                         val responseBody = response.body()?.toString()
                         Log.d("SubjectStatusFragment", "Response Body: $responseBody")
                     }
                 } else {
-                    // Handle error
                     Log.e("SubjectStatusFragment", "Error: ${response.code()}")
                 }
             } catch (e: Exception) {
-                // Handle exception
                 Log.e("SubjectStatusFragment", "Exception: ${e.message}", e)
+                teammateNameAdapter.updateData(emptyList())
             }
         }
     }
+
 }
