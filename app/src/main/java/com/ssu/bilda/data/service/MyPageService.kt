a7c7794c748229b522490c876ddf95f1f99d2255
hyo-limilimee <1001lily@naver.com>
2023-12-04 20:47:51 +0900

    [refactor] api 형식 변경

--- app/src/main/java/com/ssu/bilda/data/service/MyPageService.kt
+++ app/src/main/java/com/ssu/bilda/data/service/MyPageService.kt
@@ -1,7 +1,7 @@
 package com.ssu.bilda.data.service
 
+import com.ssu.bilda.data.remote.response.MyPageReferResponse
 import com.ssu.bilda.data.remote.response.MyPageResponse
-import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -11,5 +11,5 @@ interface MyPageService {
     suspend fun getMyPage(): Response<MyPageResponse>
 
     @GET("/api/v1/page/{userId}")
-    suspend fun getMyPageRefer(@Path("userId") userId: Long): MyPageResponse
+    suspend fun getMyPageRefer(@Path("userId") userId: Long): Response<MyPageReferResponse>
 }
